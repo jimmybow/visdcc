93612fb232061e146394df06ac3f9b196f505901
jimmybow <jimmybow@hotmail.com.tw>
2017-08-11 14:42:15 +0800

    'update'

--- src/components/Network.react.js
+++ src/components/Network.react.js
@@ -64,7 +64,8 @@ Network.defaultProps = {
            edges :[{from: 1, to: 3},
                    {from: 1, to: 2},
                    {from: 2, to: 4},
-                   {from: 2, to: 5} ]          }    
+                   {from: 2, to: 5} ]          },
+    options : {}                
 }        
 
          
